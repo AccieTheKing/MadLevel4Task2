79dd65ef80b1dc63cf6ac90f85645ee2550066e0
Acdaling <acdaling@gmail.com>
2020-10-04 14:44:35 +0200

    The Game data class can now hold the correct values

--- app/src/main/java/com/example/madlevel4example2/model/Game.kt
+++ app/src/main/java/com/example/madlevel4example2/model/Game.kt
@@ -3,18 +3,17 @@ package com.example.madlevel4example2.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import com.example.madlevel4example2.enums.GameState
-import com.example.madlevel4example2.enums.Moves
 
 @Entity(tableName = "gameTable")
 data class Game(
     @ColumnInfo(name = "state")
     var state: GameState,
     @ColumnInfo(name = "date")
-    var date: null,
+    var date: Long,
     @ColumnInfo(name = "computerMove")
-    var computerMove: Moves,
+    var computerMove: String,
     @ColumnInfo(name = "playerMove")
-    var playerMove: Moves
+    var playerMove: String
 )
 
 
