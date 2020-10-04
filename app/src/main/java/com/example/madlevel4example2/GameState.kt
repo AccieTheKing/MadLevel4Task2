cdc604bfdfa1d89cfc763109ca41b2a8bca54737
Acdaling <acdaling@gmail.com>
2020-10-04 02:14:52 +0200

    Created two enum classes

---
+++ app/src/main/java/com/example/madlevel4example2/GameState.kt
@@ -0,0 +1,5 @@
+package com.example.madlevel4example2
+
+enum class GameState {
+    WIN, DRAW, LOSE
+}
---
+++ app/src/main/java/com/example/madlevel4example2/Moves.kt
@@ -0,0 +1,5 @@
+package com.example.madlevel4example2
+
+enum class Moves {
+    ROCK, PAPER, SCISSORS
+}
