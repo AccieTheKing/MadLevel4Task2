50cd1c4571cb17e3f210f236cea512f56da37102
Acdaling <acdaling@gmail.com>
2020-10-04 02:41:09 +0200

    Programmed method that checks if user won a game

--- app/src/main/java/com/example/madlevel4example2/FirstFragment.kt
+++ app/src/main/java/com/example/madlevel4example2/FirstFragment.kt
@@ -1,12 +1,11 @@
 package com.example.madlevel4example2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,8 +13,8 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -24,8 +23,92 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        initView()
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+
+    /**
+     * This method will set the listeners
+     */
+    private fun initView() {
+        imv_rock.setOnClickListener { playGame(Moves.ROCK) }
+        imv_paper.setOnClickListener { playGame(Moves.PAPER) }
+        imv_scissors.setOnClickListener { playGame(Moves.SCISSORS) }
+    }
+
+    /**
+     * This method will launch the game
+     */
+    private fun playGame(userMove: Moves) {
+        when (userMove) {
+            Moves.ROCK -> imv_move_player.setImageResource(R.drawable.rock)
+            Moves.PAPER -> imv_move_player.setImageResource(R.drawable.paper)
+            Moves.SCISSORS -> imv_move_player.setImageResource(R.drawable.scissors)
         }
+
+        val movesByComputer = randomMoveComputer()
+
+        when (checkForWin(movesByComputer, userMove)) {
+            GameState.WIN -> {
+                txt_game_result.text = "You win"
+            }
+            GameState.DRAW -> {
+                txt_game_result.text = "Draw"
+            }
+            GameState.LOSE -> {
+                txt_game_result.text = "You lose"
+            }
+        }
+    }
+
+    /**
+     * This method will do a random move for the computer and returns the move of the computer
+     */
+    private fun randomMoveComputer(): Moves {
+        val randomNumber = (0 until 3).random()
+
+        when (numberToMove(randomNumber)) {
+            Moves.ROCK -> imv_move_computer.setImageResource(R.drawable.rock)
+            Moves.PAPER -> imv_move_computer.setImageResource(R.drawable.paper)
+            Moves.SCISSORS -> imv_move_computer.setImageResource(R.drawable.scissors)
+        }
+
+        return numberToMove(randomNumber)
+    }
+
+    /**
+     * This method will transform a number into a Move
+     */
+    private fun numberToMove(number: Int): Moves {
+        if (number == 0) {
+            return Moves.ROCK
+        } else if (number == 1) {
+            return Moves.PAPER
+        }
+
+        return Moves.SCISSORS
+    }
+
+    /**
+     * This method will check who has won the round
+     */
+    private fun checkForWin(computerMove: Moves, userMove: Moves): GameState {
+        if (computerMove == userMove) {
+            return GameState.DRAW
+        } else if (computerMove == Moves.ROCK && userMove == Moves.PAPER ||
+            computerMove == Moves.PAPER && userMove == Moves.SCISSORS ||
+            computerMove == Moves.SCISSORS && userMove == Moves.ROCK
+        ) {
+            return GameState.WIN
+        } else if (userMove == Moves.ROCK && computerMove == Moves.PAPER ||
+            userMove == Moves.PAPER && computerMove == Moves.SCISSORS ||
+            userMove == Moves.SCISSORS && computerMove == Moves.ROCK
+        ) {
+            return GameState.LOSE
+        }
+
+        return GameState.LOSE;
     }
 }
