76f4e8b739e7ba65f62fa8d8889f898233c80d88
Acdaling <acdaling@gmail.com>
2020-10-04 18:05:49 +0200

    Added the GameStateConverter to the database

--- app/src/main/java/com/example/madlevel4example2/database/GameListRoomDatabase.kt
+++ app/src/main/java/com/example/madlevel4example2/database/GameListRoomDatabase.kt
@@ -6,12 +6,13 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.madlevel4example2.converters.DateConverter
+import com.example.madlevel4example2.converters.GameStateConverter
 import com.example.madlevel4example2.converters.MovesConverter
 import com.example.madlevel4example2.dao.GameDao
 import com.example.madlevel4example2.model.Game
 
 @Database(entities = [Game::class], version = 1, exportSchema = false)
-@TypeConverters(DateConverter::class, MovesConverter::class)
+@TypeConverters(DateConverter::class, MovesConverter::class, GameStateConverter::class)
 abstract class GameListRoomDatabase : RoomDatabase() {
     abstract fun gameDao(): GameDao
 
