326631d390535ec9f5e3c4b86231159a6867c315
Acdaling <acdaling@gmail.com>
2020-10-06 16:43:07 +0200

    Created a method that converts a string to an Image resource id
    - added the correct images to the game moves

--- app/src/main/java/com/example/madlevel4example2/ui/GameListAdapter.kt
+++ app/src/main/java/com/example/madlevel4example2/ui/GameListAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel4example2.R
 import com.example.madlevel4example2.converters.DateConverter
 import com.example.madlevel4example2.converters.GameStateConverter
+import com.example.madlevel4example2.enums.Moves
 import com.example.madlevel4example2.model.Game
 import kotlinx.android.synthetic.main.item_game.view.*
 
@@ -18,6 +19,8 @@ class GameListAdapter(private val games: List<Game>) :
             itemView.txt_game_status.text =
                 GameStateConverter().stringToGameState(game.state).toString()
             itemView.txt_date.text = DateConverter().fromTimestamp(game.date).toString()
+            itemView.img__his_computer.setImageResource(movesToImgConverter(game.computerMove))
+            itemView.img_his_you.setImageResource(movesToImgConverter(game.playerMove))
         }
     }
 
@@ -33,5 +36,14 @@ class GameListAdapter(private val games: List<Game>) :
 
     override fun getItemCount(): Int {
         return games.size
+    }
+
+    private fun movesToImgConverter(move: String): Int {
+        when (move) {
+            Moves.ROCK.toString() -> return R.drawable.rock
+            Moves.PAPER.toString() -> return R.drawable.paper
+        }
+
+        return R.drawable.scissors
     }
 }
